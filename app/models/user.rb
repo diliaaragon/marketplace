0eaa23eedfc661b4375521eee173015fc031d916
diliaaragon <dilia.aragon@koombea.com>
2019-04-02 10:23:51 -0500

    Create the user show and add some css styles with bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :fist_name, presence: true, length: { minimum: 2, maximum: 25 } 
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 25 } 
   validates :last_name, presence: true, length: { minimum: 2, maximum: 25 } 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
