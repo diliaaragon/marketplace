110fab3556ddd4e6ab596027ffa9fd00dcdf3496
diliaaragon <dilia.aragon@koombea.com>
2019-04-03 10:12:04 -0500

    Add style to the views and change the format of the regular expression of the email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 2, maximum: 25 } 
   validates :last_name, presence: true, length: { minimum: 2, maximum: 25 } 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[a-z\d\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
-  before_save {self.email = email.downcase}
 end
