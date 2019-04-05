7dd2c4e0964e9e4e80916dfb734fb608acafbece
diliaaragon <dilia.aragon@koombea.com>
2019-04-05 10:00:07 -0500

    Create views to create the products

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true.
+# frozen_string_literal: true
 Rails.application.routes.draw do
   root 'users#index'
   resources :products
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products
 end
