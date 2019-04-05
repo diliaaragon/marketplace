c196a67e5380ee6fa5d6655a4ba4bb7aed40ede4
diliaaragon <dilia.aragon@koombea.com>
2019-04-05 09:37:33 -0500

    Create view/controller to list the products

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :products
   resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products
 end
