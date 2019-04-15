075ddbae5376e780c1d7936dd8d14a071cd97c10
diliaaragon <dilia.aragon@koombea.com>
2019-04-15 16:29:31 -0500

    Add the category model with its validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_04_12_201555) do
+=======
+ActiveRecord::Schema.define(version: 2019_04_15_211417) do
+>>>>>>> Add the category model with its validations
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +38,12 @@ ActiveRecord::Schema.define(version: 2019_04_12_201555) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
