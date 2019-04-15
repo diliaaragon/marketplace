32ee67713b2692e9ff188d5b43fbf23388fb94b5
diliaaragon <dilia.aragon@koombea.com>
2019-04-15 17:11:13 -0500

    Create a relationship between categories and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@
 
 # Product model with its respective validations.
 class Product < ApplicationRecord
+  has_and_belongs_to_many :categories
+
   validates :name, presence: true, length: { minimum: 3, maximum: 15 }
   validates :description, presence: true, length: { minimum: 10 }
   validates :quantity, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_211417) do
+ActiveRecord::Schema.define(version: 2019_04_15_220414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2019_04_15_211417) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.bigint "category_id"
+    t.bigint "product_id"
+    t.index ["category_id"], name: "index_categories_products_on_category_id"
+    t.index ["product_id"], name: "index_categories_products_on_product_id"
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name", null: false
     t.string "description", null: false
@@ -70,5 +77,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_211417) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "categories_products", "categories"
+  add_foreign_key "categories_products", "products"
   add_foreign_key "products", "users"
 end
