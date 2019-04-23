51aca7a94c827c8fd8c1e577ef688b15dd2f8fe5
diliaaragon <dilia.aragon@koombea.com>
2019-04-23 11:20:01 -0500

    Add status to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_05_20_193345) do
-=======
-ActiveRecord::Schema.define(version: 2019_04_23_152842) do
->>>>>>> Activate active storage
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
