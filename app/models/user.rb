5bc08154598699cd8da94787556f40d381181b92
diliaaragon <dilia.aragon@koombea.com>
2019-04-24 09:37:46 -0500

    Remove duplicated association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   validates :first_name, presence: true, length: { minimum: 2, maximum: 25 }
   validates :last_name, presence: true, length: { minimum: 2, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[a-z\d\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, length: { maximum: 100 },
                     format: { with: VALID_EMAIL_REGEX }
-  has_many :products
 end
