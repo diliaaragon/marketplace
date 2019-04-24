a43b1e88743ab8ea5d452fb2d85079636e02a427
diliaaragon <dilia.aragon@koombea.com>
2019-04-24 10:59:26 -0500

    Fix active storage configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_211417) do
+ActiveRecord::Schema.define(version: 2019_04_23_152842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_211417) do
     t.float "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "unpublished"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
