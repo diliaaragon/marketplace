85dcb467504f0e0b90872013f726e3b59edda43d
diliaaragon <dilia.aragon@koombea.com>
2019-05-13 11:33:40 -0500

    Add mailer to send emails when a product is published

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,16 @@ Rails.application.configure do
   # config.require_master_key = true
 
   #MAil
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'dilia.aragon@koombea.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            '<dilia>',
+    password:             '<3016783874>',
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
   config.action_mailer.perform_deliveries = true
   Rails.application.routes.default_url_options = { host: ENV['HOST'] }
 
@@ -74,8 +83,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "marketplace_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_options = { :host => "localhost:3000" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
