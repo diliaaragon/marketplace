7e3c711fca25d4e8f45d0fbb92a3e8b2b86b83d5
diliaaragon <dilia.aragon@koombea.com>
2019-05-14 11:08:37 -0500

    Add the profile picture to the navbar and the user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :cellphone,
-                                 :address)
+                                 :address, :photo)
   end
 end
