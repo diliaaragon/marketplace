6f17a9edaf3025654461f613e71b412fd98f4ff4
diliaaragon <dilia.aragon@koombea.com>
2019-05-20 16:52:54 -0500

    Add authentication for administrator users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
+  devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   get 'welcome/index'
