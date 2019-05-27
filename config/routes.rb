f4071f0a8b4b52a473902385630646f8ad5c7a9e
diliaaragon <dilia.aragon@koombea.com>
2019-05-27 11:31:20 -0500

    Add the option to log in with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'welcome/index'
   root 'welcome#index'
   put "/products/:id/publish", to: "products#publish"
