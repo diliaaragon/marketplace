6b1710937de491291af27ae252c51f5fe5e52b97
diliaaragon <dilia.aragon@koombea.com>
2019-05-30 17:30:36 -0500

    Add test at category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  it 'is valid with name' do
+    category = build(:category)
+    expect(category).to be_valid
+  end
+
+  it 'is invalid without name' do
+    category = build(:category, name: nil)
+    expect(category).to_not be_valid
+  end
+
+  it ' is invalid with a duplicate name' do
+    category1 = create(:category, name: 'cosmetic')
+    category = build(:category, name: 'cosmetic')
+    expect(category).to_not be_valid
+  end
+end
