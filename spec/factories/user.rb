888a3362c7fe3a01da192bef3e29e8438317066d
diliaaragon <dilia.aragon@koombea.com>
2019-06-11 16:56:22 -0500

    Change the tests using shoulda-matchers

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name  { Faker::Name.last_name }
     email { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,73 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
- 
-  it 'is valid with first name, last name, email and password' do 
-    user = build(:user)
-    expect(user).to be_valid
+
+  describe 'validations' do
+    subject { FactoryBot.create(:user) }
+
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_length_of(:first_name).is_at_least(2).is_at_most(25) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_length_of(:last_name).is_at_least(2).is_at_most(25) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to validate_length_of(:email).is_at_most(100) }
   end
 
-  it 'is invalid without a first name' do
-    user = build(:user, first_name: nil)
-    expect(user).to_not be_valid
-  end
-
-  it 'is invalid with a first name length less than 2 characters' do
-    user = build(:user, first_name: 'a')
-    expect(user).to_not be_valid
-  end
-
-  it 'is invalid with a first name length greater than 25 characters' do
-    user = build(:user, first_name:'dilia jaidith aragon aleman')
-    expect(user).to_not be_valid
-  end
-
-  it 'is invalid without a last name' do
-    user = build(:user, last_name: nil)
-    expect(user).to_not be_valid
-  end  
-
-  it 'is invalid with a last name length less than 2 characters' do
-    user = build(:user, last_name: 'i')
-    expect(user).to_not be_valid
-  end
-
-  it 'is invalid with a last name length greater than 25 characters' do
-    user = build(:user, last_name:'dilia jaidith aragon aleman')
-    expect(user).to_not be_valid
-  end
-
-  it 'is invalid without a email addess' do
-    user = build(:user, email: nil)
-    expect(user).to_not be_valid
-  end
-
-  context 'is invalid if email does not match the format' do
-    it 'is invalid without @' do
-      user = build(:user, email: 'dgmail.com')
-      expect(user).to_not be_valid
-    end
-  
-    it 'is invalid without .' do
-      user = build(:user, email: 'd@gmailcom')
-      expect(user).to_not be_valid
-    end
-
-    it 'is invalid with anything before @' do
-      user = build(:user, email: '@gmail.com')
-      expect(user).to_not be_valid
-    end
-  end
-
-  it 'is invalid with a email length greater than 100 characters' do
-    user = build(:user, email: 'svjgfyjdsgvfjdsgvjgsdkvdsadsdsafafasfasfasfasasfsafa
-                                gdfgdhfvfvhxgfikjh@gmail.com')
-    expect(user).to_not be_valid
-  end
-
-  it 'is invalid with a duplicate email address' do
-    user1 = create(:user, email: 'dj@gmail.com')
-    user = build(:user, email: 'dj@gmail.com')
-    expect(user).to_not be_valid
+  describe 'relations' do
+    it { is_expected.to have_many(:products) }
   end
 end
