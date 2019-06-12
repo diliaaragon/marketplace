b55823ce42edf310e2a73827686b4ac49cb14b91
diliaaragon <dilia.aragon@koombea.com>
2019-06-12 10:20:00 -0500

    Change the tests using shoulda-matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,19 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it 'is valid with name' do
-    category = build(:category)
-    expect(category).to be_valid
+
+  describe 'validates' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
   end
 
-  it 'is invalid without name' do
-    category = build(:category, name: nil)
-    expect(category).to_not be_valid
-  end
-
-  it ' is invalid with a duplicate name' do
-    category1 = create(:category, name: 'cosmetic')
-    category = build(:category, name: 'cosmetic')
-    expect(category).to_not be_valid
+  describe 'relations' do
+    it { is_expected.to have_and_belong_to_many(:products) }
   end
 end
