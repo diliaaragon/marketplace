9df99d76f7083a904196145a24bf484d887742e1
diliaaragon <dilia.aragon@koombea.com>
2019-06-13 14:29:04 -0500

    Change the tests using shoulda-matchers

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,88 +1,74 @@
 require 'rails_helper'
-require 'spec_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-  before do
-    @category = create(:category)
-  end
+
+  let(:category) { create(:category) }
 
   describe 'GET index' do
-    it 'ok'  do
-      get :index
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template(:index)
-      expect(assigns(:categories)).to eq([@category])
-    end
+    subject { get :index }
+
+    before { subject }
+    it { expect(response).to render_template(:index) }
+    it { expect(assigns(:categories)).to eq([category]) }
   end
 
   describe 'GET show' do
-    it 'ok' do
-      get :show, params: { id: @category.id }
-      expect(response).to have_http_status(:ok)
-      expect(response).to render_template(:show)
-      expect(assigns(:category)).to eq(@category)
+
+    context 'when category exist' do
+      subject { get :show, params: { id: category.id } }
+
+      before { subject }
+      it { expect(response).to render_template(:show) }
+      it { expect(assigns(:category)).to eq(category) }
     end
 
-    it "when category doesn't exist" do
-      expect { get :show, params: { id: 69696 } }
-        .to raise_error(ActiveRecord::RecordNotFound)
+    context "when category doesn't exist" do
+      it { expect { get :show, params: { id: 69696 } }
+        .to raise_error(ActiveRecord::RecordNotFound) }
     end
   end
 
   describe 'GET new' do
-    it 'ok' do
-      get :new
-      expect(assigns(:category)).to be_a_new(Category)
-    end
+    let(:category) { build(:category) }
+    subject { get :new }
+
+    before { subject }
+    it { expect(assigns(:category)).to be_a_new(Category) }
   end
 
   describe 'GET edit' do
-    it 'ok' do
-      get :edit, params: { id: @category.id }
-      expect(assigns(:category)).to eq(@category)
-    end
+    let(:params) { { id: category.id } }
+    subject { get :edit, params: params }
+
+    before { subject }
+    it { expect(assigns(:category)).to eq(category) }
   end
 
   describe 'POST create' do
-    before do
-      @category = create(:category)
-      @attributes = attributes_for(:category)
+    let(:params) { attributes_for(:category) }
+    subject { post :create, params: { category: params } }
+
+    context 'create a new category'  do
+      it { expect { subject }.to change(Category, :count).by(1) }
     end
 
-    context 'with valid params' do
-      it 'create a new category'  do
-        Category.new
-        expect { post :create, params: { category: @attributes } }.to change(Category, :count).by(1)
-      end
+    context 'assigns a newly created category as @category' do
+      before { subject }
+      it { expect(assigns(:category)).to be_a(Category) }
+    end
 
-      it 'assigns a newly created category as @category' do
-        post :create, params: { category: @attributes }
-        expect(assigns(:category)).to be_a(Category)
-      end
-
-      it 'redirects to the create category' do
-        post :create, params: { category: @attributes }
-        expect(response).to redirect_to(categories_path)
-      end
+    context 'redirects to the create category' do
+      before { subject }
+      it { expect(response).to redirect_to(categories_path) }
     end
   end
 
   describe 'PUT update' do
-    before do
-      @@category = create(:category)
-      @attributes = { name: 'newcategory' }
-    end
+    let(:params) { { id: category.id, category: { name: 'newname' } } }
+    subject { put :update, params: params }
 
-    context 'with valid params' do
-      it 'assigns the product as @product' do
-        put :update, params: { id: @category.id, category: @attributes }
-        expect(assigns(:category).name).to eq @attributes[:name]
-      end
-
-      it 'redirects' do
-        put :update, params: { id: @category.id, category: @attributes }
-        expect(response).to redirect_to(categories_path)
-      end
-    end
+    before { subject }
+    it { expect(assigns(:category).name).to eq('newname') }
+    it { expect(response).to redirect_to(categories_path) }
   end
 end
